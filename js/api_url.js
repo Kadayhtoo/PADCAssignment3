965eb9653cf34fcc513c8e5b71d93c3c4eef438e
ThanKaday <thankaday.htoo@mm-digital-solutions.com>
2019-02-16 11:39:43 +0630

    update

--- js/api_url.js
+++ js/api_url.js
@@ -1,7 +1,7 @@
 const rootURL = "https://api.themoviedb.org/3/";
 const upcoming = "movie/upcoming?api_key=<<api_key>>&language=en-US&page=<<page>>";
 const detail = "/movie/{movie_id}/videos?api_key=<<api_key>>";
-const key = "39a81e69f5a6260b8083e7cace35f28a";
+const key = "450506481f0397731cf00758f47f93c9";
 
 function getUpcomingApiUrl(key){
     return rootURL + upcoming.replace("<<api_key>>",key);
